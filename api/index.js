3bdb07fb3346ef1c69933176468df7f90dab38b8
faseemnaufal <faseemnaufal@gmail.com>
2024-02-09 11:08:22 +0530

    latest

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 const salt = bcrypt.genSaltSync(10)
 const secret = 'asdasdasdadadasda'
 
-app.use(cors({credentials:true, origin:'http://localhost:3000'}))
+app.use(cors({credentials:true, origin:'https://cvclient.onrender.com/'}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
--- client/src/Header.js
+++ client/src/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext)
 
   useEffect(() =>{
-    fetch('http://localhost:4000/profile', {
+    fetch('https://cvserver-3tbg.onrender.com/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -17,7 +17,7 @@ export default function Header() {
   }, [])
 
   function logout(){
-    fetch('http://localhost:4000/logout' , {
+    fetch('https://cvserver-3tbg.onrender.com/logout' , {
       credentials: 'include',
       method: 'POST',
     })
